25231d6d05680c277aa4f7cb712a02618a1ab65c
Nicholas Carpenedo <ncarpenedo@berkeley.edu>
2025-01-10 16:44:08 -0800

    Clean up GPIO lib

--- src/main/lib/manual_gpio.c
+++ src/main/lib/manual_gpio.c
@@ -3,27 +3,21 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "soc/gpio_reg.h"
-#include "bitbang_i2c.h"
+#include "manual_gpio.h"
 
 inline void gpio_enable(uint32_t pin) {
-    printf("GPIO REG: %lx\n", *((volatile uint32_t *)GPIO_ENABLE_REG));
     uint32_t bitmask = 1UL << pin;
     *((volatile uint32_t *)GPIO_ENABLE_REG) |= (uint32_t) bitmask;
-    printf("GPIO REG: %lx\n", *((volatile uint32_t *)GPIO_ENABLE_REG));
 }
 
 void gpio_set(uint32_t pin) {
-    printf("GPIO REG: %lx\n", *((volatile uint32_t *)GPIO_ENABLE_REG));
     uint32_t bitmask = 1UL << pin;
     volatile uint32_t* gpio_reg = (uint32_t*) GPIO_OUT_REG;
     *gpio_reg |= bitmask;
-    printf("GPIO REG: %lx\n", *((volatile uint32_t *)GPIO_ENABLE_REG));
 }
 
 void gpio_clear(uint32_t pin) {
-    printf("GPIO REG: %lx\n", *((volatile uint32_t *)GPIO_ENABLE_REG));
     uint32_t bitmask = 1UL << pin;
     volatile uint32_t* gpio_reg = (uint32_t*) GPIO_OUT_REG;
     *gpio_reg &= ~bitmask;
-    printf("GPIO REG: %lx\n", *((volatile uint32_t *)GPIO_ENABLE_REG));
 }
--- src/main/main.c
+++ src/main/main.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "bitbang_i2c.h"
-
-
-#include "driver/gpio.h"
-#include "soc/gpio_reg.h"
+#include "lib/manual_gpio.h"
 
 #define LED_PIN 2
 
 void app_main(void)
 {
-    uint32_t bitmask = 1UL << LED_PIN;
-    *((volatile uint32_t *)GPIO_ENABLE_REG) |= (uint32_t) bitmask;
     gpio_enable(LED_PIN);
     for(;;) {
         gpio_set(LED_PIN);
